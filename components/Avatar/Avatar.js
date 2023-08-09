9ab97dcd607c3ca13708ac922e83af5ddceed274
Pedro Martins <pedro.martins@imagen.io>
2023-08-09 13:55:31 +0100

    Avatar component changes

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -4,29 +4,89 @@ export class iwAvatar extends HTMLElement {
     constructor(){
         super();
 
-        this.attachShadow({ mode: 'open' });
-
-        const template = document.createElement('template');
+        const template = document.createElement('TEMPLATE');
         template.innerHTML = `<div class="avatar"></div>`;
+        this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true));
+    }
 
+    connectedCallback() {
         const styles = document.createElement('style');
         styles.textContent = `
             .avatar {
-                width: 52px;
-                height: 52px;
-                display: flex;
                 align-items: center;
-                justify-content: center;
-                background-color: rebeccapurple;
+                background-color: #2a2e2f;
                 border-radius: 50%;
-                font-family: sans-serif;
                 color: #fff;
-                font-weight: bold;
-                font-size: 16px;
+                display: flex;
+                justify-content: center;
+            }
+
+            .xsmall {
+                font-size: 10px;
+                height: 24px;
+                width: 24px;
+            }
+
+            .small {
+                font-size: 12px;
+                height: 32px;
+                width: 32px;
+            }
+
+            .medium {
+                font-weight: bold; 
+                height: 48px;
+                width: 48px;
+            }
+
+            .large {
+                font-size: 18px;
+                font-weight: bold; 
+                height: 56px;
+                width: 56px;
+            }
+
+            .xlarge {
+                font-size: 20px;
+                font-weight: bold; 
+                height: 64px;
+                width: 64px;
             }
         `;
 
+        this.name = this.getAttribute('name') || null;
+        this.image = this.getAttribute('image') || null;
+        this.size = this.getAttribute('size') || 'medium';
+        this.alt = this.getAttribute('alt') || 'Avatar';
+        this.tooltip = this.getAttribute('tooltip') || null;
+
+        switch(this.size) {
+            case 'xsmall':
+                this.shadowRoot.querySelector('.avatar').classList.add(this.size);
+                break;
+            case 'small':
+                this.shadowRoot.querySelector('.avatar').classList.add(this.size);
+                break;
+            case 'medium':
+                this.shadowRoot.querySelector('.avatar').classList.add(this.size);
+                break;
+            case 'large':
+                this.shadowRoot.querySelector('.avatar').classList.add(this.size);
+                break;
+            case 'xlarge':
+                this.shadowRoot.querySelector('.avatar').classList.add(this.size);
+                break;
+            default:
+                this.shadowRoot.querySelector('.avatar').classList.add('medium');
+                break;
+        }
+
+        if(this.name) {
+            this.shadowRoot.querySelector('.avatar').textContent = this.name;
+        }
+
         this.shadowRoot.appendChild(styles);
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
+
+customElements.define('iw-avatar', iwAvatar);
