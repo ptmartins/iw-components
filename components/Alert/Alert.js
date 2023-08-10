0b3e3638d8644374674d7ab123803a36958774c9
Pedro Martins <pedro.martins@imagen.io>
2023-08-10 11:37:59 +0100

    Trying to use Fontawesome in the ShadowDOM is a pain in the @ss

--- components/Alert/Alert.js
+++ components/Alert/Alert.js
@@ -4,7 +4,8 @@ export class iwAlert extends HTMLElement {
 
         const template = document.createElement('TEMPLATE');
         template.innerHTML = `
-            <div class="infoMessage">
+            <div class="alert">
+                <slot></slot>
                 <slot></slot>
             </div>
         `;
@@ -12,18 +13,68 @@ export class iwAlert extends HTMLElement {
     }
 
     connectedCallback() {
-        const component = this.shadowRoot.querySelector('.infoMessaeg');
+        const component = this.shadowRoot.querySelector('.alert');
+        const icon = this.shadowRoot.querySelector('.alert__icon');
         const styles = document.createElement('style');
         styles.textContent = `
-            .alert {
-                word-wrap: break-word;
-                word-break: break-word;
-                overflow-wrap: break-word;
-                min-width: 0;
+            :host {
+                display: block;
+                // all: initial;
                 width: 100%;
             }
 
+            .alert {
+                display: block; 
+                min-width: 0;
+                overflow-wrap: break-word;
+                padding: .75rem;
+                width: 100%;
+                word-wrap: break-word;
+                word-break: break-word;
+            }
 
+            .alert--error {
+                background-color: #fff2f0;
+            }
+
+            .alert--info {
+                background-color: #e7f4fe;
+            }
+
+            .alert--success {
+                background-color: #f6ffed;
+            }
+
+            .alert--warning {
+                background-color: #fffbe6;
+            }
+
+            ::part(icon) {
+                font-family: 'Font Awesome 6 Free';
+                font-weight: 900;
+            }
         `;
+
+        this.shadowRoot.appendChild(styles);
+
+        this.type = this.getAttribute('type');
+
+        switch(this.type) {
+            case 'info':
+                component.classList.add(`alert--${this.type}`);
+                icon.classList.add('fa-solid fa-triangle-exclamation');
+                break;
+            case 'error':
+                component.classList.add(`alert--${this.type}`)
+                break;
+            case 'success':
+                component.classList.add(`alert--${this.type}`)
+                break;
+            case 'warning':
+                component.classList.add(`alert--${this.type}`)
+                break;
+            default: 
+                break;
+        }
     }
 }
